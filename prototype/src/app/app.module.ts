084220d5e04256b56c0d75e5801c7b52133ab80c
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-04-29 20:34:14 -0300

    criado header do home

--- prototype/src/app/app.module.ts
+++ prototype/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarHomeComponent } from './shared/navbar-home/navbar-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarHomeComponent
   ],
   imports: [
     BrowserModule,
