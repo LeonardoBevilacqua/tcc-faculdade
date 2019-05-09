d624aeaf2885c62b7f6be05c029d4a70dc1d4d2b
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-05-09 19:37:57 -0300

    Finalizaçao da search page

--- prototype/src/app/core/search/search.component.ts
+++ prototype/src/app/core/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({ selector: 'app-search', templateUrl: './search.component.html', styleUrls: ['./search.component.scss'] })
+@Component({ selector: 'app-search', templateUrl: './search.component.html'})
 export class SearchComponent implements OnInit {
 
   constructor() { }
