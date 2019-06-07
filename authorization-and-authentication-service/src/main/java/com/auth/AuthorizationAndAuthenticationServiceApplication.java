e5dfe0f40cf0c7d51a1cc12142e8ca401bf778fc
Marcio Macedo <marcioluismacedo@gmail.com>
2019-06-06 21:47:12 -0300

    Liberando acesso global no CORS

--- authorization-and-authentication-service/src/main/java/com/auth/AuthorizationAndAuthenticationServiceApplication.java
+++ authorization-and-authentication-service/src/main/java/com/auth/AuthorizationAndAuthenticationServiceApplication.java
@@ -9,12 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @SpringBootApplication
 public class AuthorizationAndAuthenticationServiceApplication implements CommandLineRunner {
@@ -53,5 +54,17 @@ public class AuthorizationAndAuthenticationServiceApplication implements Command
 		usuarioService.updateSaveUsuario(u2);
 		usuarioService.updateSaveUsuario(u3);
 		usuarioService.updateSaveUsuario(u4);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
