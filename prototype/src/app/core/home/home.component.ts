1ff8d10243b3fd5a47c1047118479cf5a608027e
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-06-10 17:33:27 -0300

    UI bug fixes

--- prototype/src/app/core/home/home.component.ts
+++ prototype/src/app/core/home/home.component.ts
@@ -4,15 +4,15 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   imgs = [
-    "http://lorempixel.com/400/200/city", 
-    "http://lorempixel.com/400/200/people", 
-    "http://lorempixel.com/400/200/transport", 
-    "http://lorempixel.com/400/200/animals",
-    "http://lorempixel.com/400/200/food", 
-    "http://lorempixel.com/400/200/nature",
+    "http://lorempixel.com/400/200/business", 
+    "http://lorempixel.com/400/200/business", 
+    "http://lorempixel.com/400/200/business", 
     "http://lorempixel.com/400/200/business",
-    "http://lorempixel.com/400/200/cats",
-    "http://lorempixel.com/400/200/technics", 
+    "http://lorempixel.com/400/200/business", 
+    "http://lorempixel.com/400/200/business",
+    "http://lorempixel.com/400/200/business",
+    "http://lorempixel.com/400/200/business",
+    "http://lorempixel.com/400/200/business", 
     
   ];
   constructor() { }
