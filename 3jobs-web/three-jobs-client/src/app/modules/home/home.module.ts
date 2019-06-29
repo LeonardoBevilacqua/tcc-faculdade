119a98ab48910682b537e7a243ade5f06b104dab
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-06-29 12:02:18 -0300

    quebrando sections em components, detalhando estilo

--- 3jobs-web/three-jobs-client/src/app/modules/home/home.module.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/home/home.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
+import { HomeSectionComponent } from './components/home-section/home-section.component';
+import { AboutComponent } from './components/about/about.component';
+import { AboutSectionComponent } from './components/about-section/about-section.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeSectionComponent, AboutComponent, AboutSectionComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
--- 3jobs-web/three-jobs-client/src/app/modules/home/pages/home/home.component.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/home/pages/home/home.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
-})
+@Component({ selector: 'app-home', templateUrl: './home.component.html', styleUrls: ['./home.component.scss'] })
 export class HomeComponent implements OnInit {
 
-  constructor(private titleService: Title) { }
+	/** 
+	 * holds the current section.
+	 */
+	currentSection: string;
 
-  ngOnInit() {
-    this.titleService.setTitle(`${this.titleService.getTitle()} | Home`)
-  }
+	constructor(private titleService: Title) { }
+
+	ngOnInit() {
+		this.titleService.setTitle(`${this.titleService.getTitle()} | Home`);
+		this.currentSection = 'home';
+	}
 
 }
