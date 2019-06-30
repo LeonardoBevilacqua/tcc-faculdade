0bdbe63547d4586f2941a2088e23b38fd96a9ba0
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-06-30 20:05:05 -0300

    finalizado criaçao basica da home page

---
+++ 3jobs-web/three-jobs-client/src/app/modules/home/components/account-modal/account-modal.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({ selector: 'app-account-modal', templateUrl: './account-modal.component.html', styleUrls: ['./account-modal.component.scss'] })
+export class AccountModalComponent implements OnInit {
+
+    currentTab: string;
+    constructor() { }
+
+    ngOnInit() {
+        this.currentTab = "loginAccount";
+    }
+
+}
--- 3jobs-web/three-jobs-client/src/app/modules/home/home.module.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/home/home.module.ts
@@ -5,9 +5,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { HomeSectionComponent } from './components/home-section/home-section.component';
 import { AboutSectionComponent } from './components/about-section/about-section.component';
+import { AccountModalComponent } from './components/account-modal/account-modal.component';
 
 @NgModule({
-  declarations: [HomeComponent, HomeSectionComponent, AboutSectionComponent],
+  declarations: [HomeComponent, HomeSectionComponent, AboutSectionComponent, AccountModalComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
