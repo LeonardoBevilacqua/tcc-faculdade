11d07e14632a91b29e65a0639f69557283a4aab9
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-06-30 10:59:56 -0300

    Ajustes na navegaçao

--- 3jobs-web/three-jobs-client/src/app/modules/home/home.module.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/home/home.module.ts
@@ -4,11 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { HomeSectionComponent } from './components/home-section/home-section.component';
-import { AboutComponent } from './components/about/about.component';
 import { AboutSectionComponent } from './components/about-section/about-section.component';
 
 @NgModule({
-  declarations: [HomeComponent, HomeSectionComponent, AboutComponent, AboutSectionComponent],
+  declarations: [HomeComponent, HomeSectionComponent, AboutSectionComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
--- 3jobs-web/three-jobs-client/src/app/modules/home/pages/home/home.component.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/home/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
+declare const $: any;
+
 @Component({ selector: 'app-home', templateUrl: './home.component.html', styleUrls: ['./home.component.scss'] })
 export class HomeComponent implements OnInit {
 
@@ -12,8 +14,14 @@ export class HomeComponent implements OnInit {
 	constructor(private titleService: Title) { }
 
 	ngOnInit() {
+		// set the page title
 		this.titleService.setTitle(`${this.titleService.getTitle()} | Home`);
+		// set the active action
 		this.currentSection = 'home';
+		// collapse the navbar when a link is clicked
+		$('.nav-link').click(() => {
+			$('.navbar-collapse').collapse('hide');
+		});
 	}
 
 }
