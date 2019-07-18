83e18ba1ce1e326f2caf0e9e0c22f725f9cf7d8a
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-07-18 20:13:10 -0300

    Adicionado form

--- 3jobs-web/three-jobs-client/src/app/app-routing.module.ts
+++ 3jobs-web/three-jobs-client/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './modules/home/home.module#HomeModule', },
-  { path: 'profile', loadChildren: './modules/profile/profile.module#ProfileModule', },
+  { path: 'home', loadChildren: './modules/home/home.module#HomeModule', },
+  { path: '', loadChildren: './modules/profile/profile.module#ProfileModule', },
 ];
 
 @NgModule({
