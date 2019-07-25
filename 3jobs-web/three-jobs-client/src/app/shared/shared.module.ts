a6533c1bd76e12b40e1d9f4448b2595a126b7824
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-07-25 16:57:14 -0300

    #13 importante coreModule

--- 3jobs-web/three-jobs-client/src/app/shared/shared.module.ts
+++ 3jobs-web/three-jobs-client/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { CoreModule } from '../core/core.module';
 import { ValidationMessageComponent } from './form/components/validation-message/validation-message.component';
 
 
 @NgModule({
     declarations: [ValidationMessageComponent],
     imports: [
-        CommonModule,    
+        CommonModule,
+        CoreModule
     ],
     exports: [
         CommonModule,
