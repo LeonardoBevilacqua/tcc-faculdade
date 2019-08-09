e1a601357c0aa05778da5f8d39aadc8328af417a
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-08-09 20:42:56 -0300

    adicionado atributos na entity de teste

--- 3jobs-web/three-jobs-client/src/app/shared/models/testes.ts
+++ 3jobs-web/three-jobs-client/src/app/shared/models/testes.ts
@@ -1,4 +1,8 @@
 import { Entity } from './entity';
 
 export class Testes extends Entity {
+    exampleFormControlInput1: string;
+    exampleFormControlSelect1: number;
+    exampleFormControlSelect2: number;
+    exampleFormControlTextarea1: string;
 }
