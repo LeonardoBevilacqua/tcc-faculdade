0feb881737730cdb535d122a79d0ac404cc049a1
Thiago Oliveira <thiago.oliveira@matera.com>
2019-08-14 16:56:09 -0300

    Add header e footer

--- 3jobs-web/three-jobs-client/src/app/modules/job-details/job-details.module.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/job-details/job-details.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoreModule } from 'src/app/core/core.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { JobDetailsRoutingModule } from './job-details-routing.module';
 import { JobDetailsComponent } from './pages/job-details/job-details.component';
@@ -8,7 +10,9 @@ import { JobDetailsComponent } from './pages/job-details/job-details.component';
   declarations: [JobDetailsComponent],
   imports: [
     CommonModule,
-    JobDetailsRoutingModule
+    JobDetailsRoutingModule,
+    SharedModule,
+    CoreModule
   ]
 })
 export class JobDetailsModule { }
