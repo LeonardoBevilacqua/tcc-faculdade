bb86eac7d1048810fa1e9d2aa9523c352f26c582
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-08-17 12:48:47 -0300

    frontend#16 criado estrutura do search

--- 3jobs-web/three-jobs-client/src/app/app-routing.module.ts
+++ 3jobs-web/three-jobs-client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './modules/home/home.module#HomeModule', },
   { path: 'profile', loadChildren: './modules/profile/profile.module#ProfileModule', },
+  { path: 'search', loadChildren: './modules/search/search.module#SearchModule', },
   { path: 'testes', loadChildren: './modules/testes/testes.module#TestesModule', },
 ];
 
