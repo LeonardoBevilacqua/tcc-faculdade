ac03f2dde891f5b50fd6395e4058893ad4772665
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-08-17 18:36:43 -0300

    frontend#16 adicionado estruturas novas a pagina

--- 3jobs-web/three-jobs-client/src/app/app-routing.module.ts
+++ 3jobs-web/three-jobs-client/src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './modules/home/home.module#HomeModule', },
+  { path: 'home', loadChildren: './modules/home/home.module#HomeModule', },
   { path: 'profile', loadChildren: './modules/profile/profile.module#ProfileModule', },
-  { path: 'search', loadChildren: './modules/search/search.module#SearchModule', },
+  { path: '', loadChildren: './modules/search/search.module#SearchModule', },
   { path: 'testes', loadChildren: './modules/testes/testes.module#TestesModule', },
 ];
 
