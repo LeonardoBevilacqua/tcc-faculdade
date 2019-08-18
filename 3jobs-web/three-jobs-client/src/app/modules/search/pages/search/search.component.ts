d4d819d2759ca56b6beb01cc55f6690324b11025
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-08-18 12:50:42 -0300

    frontend#16 implementado dinamica no botao

--- 3jobs-web/three-jobs-client/src/app/modules/search/pages/search/search.component.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/search/pages/search/search.component.ts
@@ -1,15 +1,98 @@
 import { Component, OnInit } from '@angular/core';
+declare const $: any;
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+    tagsList: String[] = ["angular", "java", "backend"];
+    filterList: Object[] = [
+        {
+            titleFilter: "NÍVEL",
+            filters: [{
+                cod: 1,
+                name: "Júnior",
+                qtd: 223
+            },
+            {
+                cod: 2,
+                name: "Pleno",
+                qtd: 23
+            },
+            {
+                cod: 3,
+                name: "Sênior",
+                qtd: 2
+            }]
+        },
+        {
+            titleFilter: "CIDADE",
+            filters: [{
+                cod: 4,
+                name: "Campinas",
+                qtd: 22
+            },
+            {
+                cod: 5,
+                name: "Hortôlandia",
+                qtd: 3
+            },
+            {
+                cod: 6,
+                name: "Sumaré",
+                qtd: 2
+            }]
+        }
+    ];
 
-  ngOnInit() {
-  }
+    jobList: Object = [
+        {
+            title: "Java",
+            company: "Eldorado",
+            nivel: "Júnior",
+            city: "Campinas",
+            description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+            voluptates beatae
+            vel quam rem quos qui! Eos officia nisi pariatur laboriosam sequi blanditiis. Modi iste culpa,
+            expedita adipisci in eos!`,
+            publicated: "4 Dias"
+        },
+        {
+            title: "Java",
+            company: "Daitan",
+            nivel: "Pleno",
+            city: "Campinas",
+            description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+            voluptates beatae
+            vel quam rem quos qui! Eos officia nisi pariatur laboriosam sequi blanditiis. Modi iste culpa,
+            expedita adipisci in eos!`,
+            publicated: "02/02/2019"
+        }
+    ];
+
+    constructor() { }
+
+    ngOnInit() {
+        $(document).ready(function(){
+            $('#btn-filter-filter').click(() =>{
+                $( "#container-filters" ).removeClass('d-none');
+                $('#btn-filter-filter').addClass('d-none');
+                $('#btn-filter-cancel').removeClass('d-none');
+                $('#btn-filter-apply').removeClass('d-none');
+            });
+            $('#btn-filter-cancel').click(() =>{
+                $( "#container-filters" ).addClass('d-none');
+                $('#btn-filter-filter').removeClass('d-none');
+                $('#btn-filter-cancel').addClass('d-none');
+                $('#btn-filter-apply').addClass('d-none');
+            });
+        });
+        
+    }
+
+    
 
 }
