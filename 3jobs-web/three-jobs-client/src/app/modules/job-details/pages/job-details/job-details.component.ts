4b86bbf5b1fc9e0e8cd91076e1fd778b48d16ee7
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-08-19 17:04:37 -0300

    #17 review: removendo scss não utilizado

--- 3jobs-web/three-jobs-client/src/app/modules/job-details/pages/job-details/job-details.component.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/job-details/pages/job-details/job-details.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-job-details',
-  templateUrl: './job-details.component.html',
-  styleUrls: ['./job-details.component.scss']
-})
+@Component({ selector: 'app-job-details', templateUrl: './job-details.component.html' })
 export class JobDetailsComponent implements OnInit {
 
   constructor() { }
