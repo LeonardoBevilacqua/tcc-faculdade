e28bab715809700f7e5d1eb41caf90676e829252
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-08-24 00:10:57 -0300

    #12 Adicionado chartjs para os graficos

--- 3jobs-web/three-jobs-client/src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+    PieChart: any;
+    Doughnut: any;
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+        this.PieChart = new Chart('pieChart', {
+            type: 'pie',
+            data: {
+                labels: ["Campinas","Hotolândia", "Sumaré"],
+                datasets: [{
+                    data: [9, 7, 2],
+                    borderWidth:1
+                }]
+                
+            },
+            options: {
+                legend: {
+                    display: false,
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+                title: {
+                    text: "Cidade dos Candidatos",
+                    display: true
+                }
+            }
+        });
 
-  ngOnInit() {
-  }
+        this.Doughnut = new Chart('doughnutChart', {
+            type: 'doughnut',
+            data: {
+                labels: ["pendentes","Avaliados"],
+                datasets: [{
+                    data: [9, 7],
+                    borderWidth:1,
+                    backgroundColor:["#b0b2b5", "#21cae5"]
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+                title: {
+                    text: "Candidatos Avaliados",
+                    display: true
+                }
+            }
+        });
+    }
 
 }
