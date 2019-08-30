b561257db088ee6a169e2ee1d5d4fd9611413f23
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-08-30 20:43:22 -0300

    #58 ajuste no personal-data

--- 3jobs-web/three-jobs-client/src/app/modules/profile/profile.module.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/profile/profile.module.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 
@@ -18,5 +18,6 @@ import { ProfileRoutingModule } from './profile-routing.module';
         ProfileRoutingModule,
         SharedModule,
     ],
+    providers: [DatePipe]
 })
 export class ProfileModule { }
