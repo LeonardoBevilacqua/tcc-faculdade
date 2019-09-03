3b9a906d8bba874583f321b9ce64176ecefb6c52
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-09-03 19:44:56 -0300

    Alterado GenerationType.AUTO -> GenerationType.IDENTITY, alteração no join the profile e experiences, remoção de name em user

--- services/core-service/src/main/java/com/core/model/Rate.java
+++ services/core-service/src/main/java/com/core/model/Rate.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 public class Rate {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String status;
