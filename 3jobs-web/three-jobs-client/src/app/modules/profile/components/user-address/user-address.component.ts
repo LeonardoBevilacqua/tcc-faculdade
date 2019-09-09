0bc845340f949a39cb7a6400de7da5240aa8bf30
Leonardo Almeida Bevilacqua <leonardo.bevilacqua@eldorado.org.br>
2019-09-09 13:38:28 -0300

    #72 profile fixes

--- 3jobs-web/three-jobs-client/src/app/modules/profile/components/user-address/user-address.component.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/profile/components/user-address/user-address.component.ts
@@ -2,11 +2,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 import { ToastrService } from 'ngx-toastr';
-import { MaintainForm } from 'src/app/shared/form/maintain-form';
-import { ProfileService } from 'src/app/core/services/profile.service';
-import { Profile } from 'src/app/shared/models/profile';
-import { User } from 'src/app/shared/models/user';
 import { UserService } from 'src/app/core/services/user.service';
+import { MaintainForm } from 'src/app/shared/form/maintain-form';
+import { User } from 'src/app/shared/models/user';
+import { Address } from 'src/app/shared/models/address';
 
 @Component({ selector: 'user-address', templateUrl: './user-address.component.html' })
 export class UserAddressComponent extends MaintainForm<User> implements OnInit {
@@ -33,10 +32,13 @@ export class UserAddressComponent extends MaintainForm<User> implements OnInit {
     ngOnInit() {
         this.isFormEdition = false;
         this.isEdition = true;
-        
+
         this.model = this.user;
+
+        if (!this.model.profile.address) {
+            this.model.profile.address = new Address();
+        }
 
         this.getCurrentId();
     }
-
 }
