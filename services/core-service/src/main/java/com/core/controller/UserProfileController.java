1501441ebd9be3eddd6bdb75f187e99ad718e48f
Marcio Macedo <marcioluismacedo@gmail.com>
2019-09-14 16:14:01 -0300

    Profiles endpoint

---
+++ services/core-service/src/main/java/com/core/controller/UserProfileController.java
@@ -0,0 +1,25 @@
+package com.core.controller;
+
+import com.core.model.Profile;
+import com.core.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/profile")
+public class UserProfileController {
+
+    @Autowired
+    private ProfileService profileService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProfile(@PathVariable Long id) {
+        return ResponseEntity.ok(profileService.getProfile(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProfile(@PathVariable Long id, @RequestBody Profile profile) {
+        return ResponseEntity.ok(profileService.updateProfile(id, profile));
+    }
+}
