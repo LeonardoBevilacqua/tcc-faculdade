f96236272c4d9d93c76c1bb953d9e8db114a3132
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-09-26 16:06:34 -0300

    #52 transformado todoList em componente

--- 3jobs-web/three-jobs-client/src/app/modules/dashboard/dashboard.module.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/dashboard/dashboard.module.ts
@@ -7,9 +7,10 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CreateVacancyModalComponent } from './components/create-vacancy-modal/create-vacancy-modal.component';
 import { JobDashboardComponent } from './pages/job-dashboard/job-dashboard.component';
 import { CardDashboardComponent } from './components/card-dashboard/card-dashboard.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
 
 @NgModule({
-  declarations: [DashboardComponent, CreateVacancyModalComponent, JobDashboardComponent, CardDashboardComponent],
+  declarations: [DashboardComponent, CreateVacancyModalComponent, JobDashboardComponent, CardDashboardComponent, ToDoListComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
