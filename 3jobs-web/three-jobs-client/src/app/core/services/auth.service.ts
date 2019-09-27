e395053d8fcaff8a126fef1a3dca2b22d4ba65c4
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-09-27 17:00:33 -0300

    #78 Login salvar usuario

--- 3jobs-web/three-jobs-client/src/app/core/services/auth.service.ts
+++ 3jobs-web/three-jobs-client/src/app/core/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpResponseBase } from '@angular/common/http';
+import { HttpClient, HttpResponseBase, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -22,27 +22,39 @@ export class AuthService extends EntityService<User>{
             `${this.apiUrl}/${this.endpoint}`,
             { email: user.email, password: user.password },
             { observe: 'response' }
-        )
-            .pipe(map((response: HttpResponseBase) => {
-                // login successful if there's a jwt token in the response header
-                const token = response && response.headers.get('Authorization');
-                if (token) {
-                    // store email and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('user', JSON.stringify({ email: user.email, token }));
+        ).pipe(map((response) => {
+            // login successful if there's a jwt token in the response header
+            const token = response && response.headers.get('Authorization');
+            if (token) {
+                // store email and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('userToken', JSON.stringify({ email: user.email, token }));
 
-                    // return true to indicate successful login
-                    return true;
-                }
-                else {
-                    // return false to indicate failed login
-                    return false;
-                }
-            }));
+                // store user
+                localStorage.setItem('user', JSON.stringify({ user: response.body }));
+
+                // return true to indicate successful login
+                return true;
+            }
+            else {
+                // return false to indicate failed login
+                return false;
+            }
+        }));
+    }
+
+    public logout() {
+        localStorage.clear();
     }
 
     public getToken() {
-        const user = JSON.parse(localStorage.getItem('user'));
+        const userToken = JSON.parse(localStorage.getItem('userToken'));
 
-        return user != null ? user.token : null;
+        return userToken != null ? userToken.token : null;
+    }
+
+    public getUserRole() {
+        const user: User = JSON.parse(localStorage.getItem('user'));
+
+        return user != null && user.roles != null ? user.roles : null;
     }
 }
