e9fcc1a636012f8d408926801940cd6ac7e3a352
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-09-27 16:14:36 -0300

    #52 card-dashboard recebe parametros

--- 3jobs-web/three-jobs-client/src/app/modules/dashboard/components/card-dashboard/card-dashboard.component.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/dashboard/components/card-dashboard/card-dashboard.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-card-dashboard',
-  templateUrl: './card-dashboard.component.html',
-  styleUrls: ['./card-dashboard.component.scss']
+    selector: 'app-card-dashboard',
+    templateUrl: './card-dashboard.component.html',
+    styleUrls: ['./card-dashboard.component.scss']
 })
 export class CardDashboardComponent implements OnInit {
 
-  constructor() { }
+    @Input() titulo: String;
+    @Input() subtitulo: String;
+    @Input() valor: Number;
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
