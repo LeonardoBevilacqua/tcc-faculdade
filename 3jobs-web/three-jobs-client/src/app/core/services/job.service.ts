d4ed5cf849b65f188344c7909d7942353005b026
thiiluh <thiluiz.oliveira@gmail.com>
2019-09-27 23:39:23 -0300

    ADD metodo register

--- 3jobs-web/three-jobs-client/src/app/core/services/job.service.ts
+++ 3jobs-web/three-jobs-client/src/app/core/services/job.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
 import { EntityService } from './entity.service';
 import { Job } from 'src/app/shared/models/job';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 /**
  * Service responsible to handle the profile.
  */
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 @Injectable()
 export class JobService extends EntityService<Job> {
-
+   
   constructor(http: HttpClient) {
       super(http, 'jobs');
    }
+
+  
+    public register(idUser: number, idJob: number): Observable<Job> {
+        return this.httpClient.put<Job>(`${this.apiUrl}/${this.endpoint}/${idJob}/register/${idUser}`, '', httpOptions);
+    }
 }
--- 3jobs-web/three-jobs-client/src/app/modules/job-details/pages/job-details/job-details.component.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/job-details/pages/job-details/job-details.component.ts
@@ -15,72 +15,85 @@ import { User } from 'src/app/shared/models/user';
 @Component({ selector: 'app-job-details', templateUrl: './job-details.component.html' })
 export class JobDetailsComponent implements OnInit {
 
-  isLoggedUserProfile: boolean;
+    isLoggedUserProfile: boolean;
 
-  job: Job;
-  user: User;
+    job: Job;
+    user: User;
+    currentId: number;
 
-  
-  constructor(
-      private titleService: Title,
-      private jobService: JobService,
-      private router: Router,
-      private toast: ToastrService,
-      private spinnerService: Ng4LoadingSpinnerService,
-      private authService: AuthService) {
+    constructor(
+        private titleService: Title,
+        private jobService: JobService,
+        private router: Router,
+        private toast: ToastrService,
+        private spinnerService: Ng4LoadingSpinnerService,
+        private authService: AuthService) {
         this.user = new User();
-      this.job = new Job();
-      this.job.company = new Company;
-      this.job.company.address = new Address;
-      this.job.tags = []
-     
-      }
-      
-
-  ngOnInit() {
-      // set the page title
-      this.titleService.setTitle(`3Jobs | Vagas`);
-
-      console.log(this.authService.getUserId())
-
-      this.spinnerService.show();
-
-      this.isLoggedUserProfile = true;
-
-      // get the current path id if exists
-      const currentId = +this.router.url.split('/')[2];
-
-      this.jobService.read(currentId).subscribe(
-          (job: Job) => {
-            console.log(job)
-              this.job = job;              
-              this.spinnerService.hide();
-          },
-          (error: HttpErrorResponse) => {
-              this.spinnerService.hide();
-
-              if (error.status === 404) {
-                  this.toast.error('A Vaga que está tentando buscar não existe!');
-              }
-              else if (error.status === 401) {
-                  this.toast.error(error.error.error);
-              }
-              else {
-                  this.toast.error('Ocorreu um erro inesperado, por favor aguarde!');
-              }
-              this.router.navigateByUrl('/');
-          }
-      );
-  }
+        this.job = new Job();
+        this.job.company = new Company;
+        this.job.company.address = new Address;
+       
+    }
 
 
+    ngOnInit() {
+        // set the page title
+        this.titleService.setTitle(`3Jobs | Vagas`);    
 
-  apply(){
+        this.spinnerService.show();
 
-    this.job.users = [];
-    this.user.id = this.authService.getUserId()
-    this.job.users.push(this.user)
-    console.log(    this.job.users)
-  }
+        this.isLoggedUserProfile = true;
+
+        // get the current path id if exists
+        this.currentId = +this.router.url.split('/')[2];
+
+        this.jobService.read(this.currentId).subscribe(
+            (job: Job) => {
+                console.log(job)
+                this.job = job;
+                this.spinnerService.hide();
+            },
+            (error: HttpErrorResponse) => {
+                this.spinnerService.hide();
+
+                if (error.status === 404) {
+                    this.toast.error('A Vaga que está tentando buscar não existe!');
+                }
+                else if (error.status === 401) {
+                    this.toast.error(error.error.error);
+                }
+                else {
+                    this.toast.error('Ocorreu um erro inesperado, por favor aguarde!');
+                }
+                this.router.navigateByUrl('/');
+            }
+        );
+    }
+
+    apply() {
+        this.spinnerService.show();
+
+        this.jobService.register(this.authService.getUserId(), this.currentId).subscribe(
+            (response: any) => {
+                this.spinnerService.hide();
+                this.toast.success('Cadastrado com sucesso!');
+                this.router.navigateByUrl('/dashboard');
+            },
+            (error: HttpErrorResponse) => {
+
+                this.spinnerService.hide();
+                if (error.status === 403) {
+                    this.toast.warning('Por gentileza, logar no sistema para se candidatar-se');
+                }
+               
+                else {
+                    this.toast.error('Ocorreu um erro inesperado, por favor aguarde!');
+                }
+              
+            }
+
+            
+        );
+    }
 
 }
