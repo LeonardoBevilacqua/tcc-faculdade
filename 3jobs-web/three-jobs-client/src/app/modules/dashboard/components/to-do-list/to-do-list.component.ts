303dbf566f1700cc93e74e59fcaaa50050599492
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2019-09-30 16:27:05 -0300

    Update to-do-list.component.ts

--- 3jobs-web/three-jobs-client/src/app/modules/dashboard/components/to-do-list/to-do-list.component.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/dashboard/components/to-do-list/to-do-list.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-to-do-list',
-  templateUrl: './to-do-list.component.html',
-  styleUrls: ['./to-do-list.component.scss']
-})
+@Component({ selector: 'app-to-do-list', templateUrl: './to-do-list.component.html' })
 export class ToDoListComponent implements OnInit {
 
   constructor() { }
