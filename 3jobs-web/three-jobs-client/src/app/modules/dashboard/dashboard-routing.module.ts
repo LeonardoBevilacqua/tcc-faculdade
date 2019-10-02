d6ee16d41094fa84909b1bf522f857c62fc27964
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-10-02 19:52:33 -0300

    #79 job-dashboard para job

--- 3jobs-web/three-jobs-client/src/app/modules/dashboard/dashboard-routing.module.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/dashboard/dashboard-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
         component: DashboardComponent,
     },
     {
-        path: 'job-dashboard',
+        path: 'job',
         component: JobDashboardComponent,
     }
 ];
