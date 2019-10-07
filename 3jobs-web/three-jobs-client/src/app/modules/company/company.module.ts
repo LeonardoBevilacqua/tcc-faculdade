1ca70b45722fddf3eb72c45981d7eb2f8984af60
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-10-07 19:11:37 -0300

    #85 criado formulário

--- 3jobs-web/three-jobs-client/src/app/modules/company/company.module.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/company/company.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { CompanyRoutingModule } from './company-routing.module';
 import { CompanyComponent } from './pages/company/company.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
     declarations: [CompanyComponent],
     imports: [
         CommonModule,
-        CompanyRoutingModule
+        CompanyRoutingModule,
+        SharedModule,
     ]
 })
 export class CompanyModule { }
--- 3jobs-web/three-jobs-client/src/app/modules/company/pages/company/company.component.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/company/pages/company/company.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { MaintainForm } from 'src/app/shared/form/maintain-form';
+import { Address } from 'src/app/shared/models/address';
+import { Company } from 'src/app/shared/models/company';
 
 @Component({ selector: 'app-company', templateUrl: './company.component.html', styleUrls: ['./company.component.scss'] })
-export class CompanyComponent implements OnInit {
+export class CompanyComponent extends MaintainForm<Company> implements OnInit {
 
-    constructor() { }
+    constructor(router: Router, toastr: ToastrService, private titleService: Title) {
+        super(null, router, toastr);
+    }
 
     ngOnInit() {
+        this.titleService.setTitle(`3Jobs | Cadastre sua empresa`);
+
+        this.model = new Company();
+        this.model.address = new Address();
     }
 
 }
