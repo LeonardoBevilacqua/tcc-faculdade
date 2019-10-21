13f71750a2e4bfe2f3a95920074d7776a2785be8
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-10-21 18:00:25 -0300

    #101 Remover botões da navbar quando deslogado. Adicionar botão para retornar ao inicio da pagina.

--- 3jobs-web/three-jobs-client/src/app/shared/header/header.component.ts
+++ 3jobs-web/three-jobs-client/src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { Router } from '@angular/router';
+import { isNullOrUndefined } from 'util';
 
 /**
  * Component responsible to display and handle the header of the page.
@@ -12,5 +13,9 @@ export class HeaderComponent{
     logout() {
         this.authService.logout();
         this.router.navigateByUrl('/');
+    }
+
+    isUserLogged(): boolean {
+        return !isNullOrUndefined(this.authService.getToken());
     }
 }
