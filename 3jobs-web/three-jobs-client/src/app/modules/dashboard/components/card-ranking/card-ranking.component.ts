cd4774eda5ad3e4772669fe7f3580f8887132fd2
capp59marcelo <capp59marcelo@hotmail.com>
2019-10-25 16:26:55 -0300

    #102 Escondendo conteudo quando ROLE for igual headhunter

--- 3jobs-web/three-jobs-client/src/app/modules/dashboard/components/card-ranking/card-ranking.component.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/dashboard/components/card-ranking/card-ranking.component.ts
@@ -1,17 +1,22 @@
-import { Score } from './../../../../shared/models/score';
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Role } from 'src/app/shared/models/enums/role.enum';
 import { Profile } from './../../../../shared/models/profile';
-import { Component, OnInit, Input } from '@angular/core';
+import { Score } from './../../../../shared/models/score';
 
 @Component({ selector: 'app-card-ranking', templateUrl: './card-ranking.component.html' })
 export class CardRankingComponent implements OnInit {
 
     @Input() ranking: Array<Score>;
     @Input() headhunter: Profile;
-    
-    constructor() { }
+
+    constructor(private authService: AuthService) { }
 
     ngOnInit() {
         this.headhunter = new Profile();
     }
 
+    shouldDisplayHeadhunter() {
+        return this.authService.getUserRole() === Role.ROLE_HEADHUNTER;
+    }
 }
