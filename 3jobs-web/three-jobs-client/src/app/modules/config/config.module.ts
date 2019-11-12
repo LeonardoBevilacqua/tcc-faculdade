58eb4c8ccfc3fd57b610bbf211f9ea7e4e2a845e
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-11-12 19:50:41 -0300

    #112 allowing configure profile

--- 3jobs-web/three-jobs-client/src/app/modules/config/config.module.ts
+++ 3jobs-web/three-jobs-client/src/app/modules/config/config.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ConfigRoutingModule } from './config-routing.module';
 import { ConfigComponent } from './pages/config/config.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CoreModule } from 'src/app/core/core.module';
 
 @NgModule({
-  declarations: [ConfigComponent],
-  imports: [
-    CommonModule,
-    ConfigRoutingModule
-  ]
+    declarations: [ConfigComponent],
+    imports: [
+        CommonModule,
+        ConfigRoutingModule,
+        SharedModule,
+        CoreModule,
+    ]
 })
 export class ConfigModule { }
