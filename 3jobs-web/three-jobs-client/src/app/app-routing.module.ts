698776999153871a831c6c0b87b4473148c36d47
capp59marcelo <capp59marcelo@hotmail.com>
2019-11-26 22:19:24 -0300

    frontend#73 implementado rota para o quiz

--- 3jobs-web/three-jobs-client/src/app/app-routing.module.ts
+++ 3jobs-web/three-jobs-client/src/app/app-routing.module.ts
@@ -4,19 +4,20 @@ import { AuthGuard } from './core/mechanism/auth.guard';
 import { LoggedInGuard } from './core/mechanism/logged-in.guard';
 
 const routes: Routes = [
-  { path: '', loadChildren: './modules/home/home.module#HomeModule', canActivate: [LoggedInGuard] },
-  { path: 'profile', loadChildren: './modules/profile/profile.module#ProfileModule', canActivate: [AuthGuard] },
-  { path: 'search', loadChildren: './modules/search/search.module#SearchModule'},
-  { path: 'testes', loadChildren: './modules/testes/testes.module#TestesModule', },
-  { path: 'job-details', loadChildren: './modules/job-details/job-details.module#JobDetailsModule', },
-  { path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule', canActivate: [AuthGuard] },
-  { path: 'register', loadChildren: './modules/register/register.module#RegisterModule', canActivate: [LoggedInGuard] },
-  { path: 'company', loadChildren: './modules/company/company.module#CompanyModule', canActivate: [AuthGuard] },
-  { path: 'config', loadChildren: './modules/config/config.module#ConfigModule', canActivate: [AuthGuard] },
+    { path: '', loadChildren: './modules/home/home.module#HomeModule', canActivate: [LoggedInGuard] },
+    { path: 'profile', loadChildren: './modules/profile/profile.module#ProfileModule', canActivate: [AuthGuard] },
+    { path: 'search', loadChildren: './modules/search/search.module#SearchModule' },
+    { path: 'testes', loadChildren: './modules/testes/testes.module#TestesModule', },
+    { path: 'job-details', loadChildren: './modules/job-details/job-details.module#JobDetailsModule', },
+    { path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule', canActivate: [AuthGuard] },
+    { path: 'register', loadChildren: './modules/register/register.module#RegisterModule', canActivate: [LoggedInGuard] },
+    { path: 'company', loadChildren: './modules/company/company.module#CompanyModule', canActivate: [AuthGuard] },
+    { path: 'config', loadChildren: './modules/config/config.module#ConfigModule', canActivate: [AuthGuard] },
+    { path: 'quiz', loadChildren: './modules/quiz/quiz.module#QuizModule'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
